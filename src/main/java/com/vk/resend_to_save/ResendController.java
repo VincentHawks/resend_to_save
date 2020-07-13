b934351974777a28e832734a842c46386a0231e1
VincentHawks <qwertar3@gmail.com>
2020-07-13 16:59:14 +0300

    Confirmation code, fetch the image schema

--- src/main/java/com/vk/resend_to_save/ResendController.java
+++ src/main/java/com/vk/resend_to_save/ResendController.java
@@ -2,22 +2,20 @@ package com.vk.resend_to_save;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ResendController {
 
     @PostMapping("/")
-    public ResponseEntity<String> respond(@RequestBody RequestModel request) {
+    public ResponseEntity<String> respond(@RequestBody String request) {
 
-        if(request.getType().equals("confirmation")) return new ResponseEntity<>("cfbbe0b5", HttpStatus.OK);
+        //if(request.getType().equals("confirmation")) return new ResponseEntity<>("cfbbe0b5", HttpStatus.OK);
 
         // Else extract the attachment and send back the copy;
-
-        System.out.println(request.getMessage().getAttachments().get(0).getImage());
+        System.out.println(request/*.getMessage().getAttachments().get(0).getImage()*/);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
