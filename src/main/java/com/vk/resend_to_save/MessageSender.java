04de46688867b5b0dfeebfebd640d25b7691a49f
VincentHawks <qwertar3@gmail.com>
2020-07-13 18:03:09 +0300

    Add access token

--- src/main/java/com/vk/resend_to_save/MessageSender.java
+++ src/main/java/com/vk/resend_to_save/MessageSender.java
@@ -23,7 +23,9 @@ public class MessageSender {
         url.append("https://api.vk.com/method/messages.send?user_id=").append(image.getOwner_id())
                 .append("&random_id=").append(random.nextLong())
                 .append("&peer_id=").append(image.getOwner_id())
-                .append("&attachment=").append(image.getIdentifier());
+                .append("&attachment=").append(image.getIdentifier())
+                .append("&access_token=a253e1632a7cbaeef7f67387e19a9c08dda0e7b14a7c3eca4e4ec6d07bb3457d81b259d45be30ae50e070")
+                .append("&v=5.50");
         String response = this.restTemplate.getForObject(url.toString(), String.class);
         System.out.println(response);
     }
