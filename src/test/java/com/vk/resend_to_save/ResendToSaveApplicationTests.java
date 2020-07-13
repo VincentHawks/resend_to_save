06e0c6e823902c331dff6aa45594cad2c0e0b8a1
VincentHawks <qwertar3@gmail.com>
2020-07-13 16:45:23 +0300

    Confirmation code, fetch the image schema

---
+++ src/test/java/com/vk/resend_to_save/ResendToSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.resend_to_save;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResendToSaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
