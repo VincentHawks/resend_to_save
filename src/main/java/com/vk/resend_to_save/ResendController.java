37616a6ab994f08c42223ebada6f50df749256a8
VincentHawks <qwertar3@gmail.com>
2020-12-27 14:42:58 +0300

    Suppress null pointer exception

--- src/main/java/com/vk/resend_to_save/ResendController.java
+++ src/main/java/com/vk/resend_to_save/ResendController.java
@@ -1,5 +1,8 @@
 package com.vk.resend_to_save;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,20 +19,28 @@ public class ResendController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<String> respond(@RequestBody RequestModel request) {
+    public ResponseEntity<String> respond(@RequestBody String requestString) throws JsonProcessingException {
 
-        if (request.getType().equals("confirmation")) return new ResponseEntity<>("e66e4554", HttpStatus.OK);
+        ObjectMapper mapper = new ObjectMapper();
+        RequestModel requestPOJO = mapper.readValue(requestString, RequestModel.class);
+
+        if (requestPOJO.getType().equals("confirmation")) return new ResponseEntity<>("e66e4554", HttpStatus.OK);
 
         // Else extract the attachment and send back the copy;
-        if(request.getMessage().getUser_id() != -197092083) { // To protect from possible loop-sending
-            for (Attachment a : request.getMessage().getAttachments()) { // Resend every attached image separately
-                if (a.getType().equals("photo")) {
-                    sender.send(a.getImage());
+        if (requestPOJO.getMessage().getUser_id() != -197092083) { // To prevent possible loop-sending
+            if (requestPOJO.getMessage().getAttachments() != null) {
+                for (Attachment a : requestPOJO.getMessage().getAttachments()) { // Resend every attached image separately
+                    if (a.getType().equals("photo")) {
+                        sender.send(a.getImage());
+                    }
                 }
             }
+            else {
+                System.out.println(requestString);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
         }
-        sender.setMessagesRead(request.getMessage().getUser_id());
+        sender.setMessagesRead(requestPOJO.getMessage().getUser_id());
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
